8c75f0d558314b8846fa7aab7d4863dee071ff7e
Jonathan Neal <jonathantneal@hotmail.com>
2015-07-07 19:58:44 -0400

    Update plugin
    Refactored for clarity with comments

--- index.js
+++ index.js
@@ -5,145 +5,95 @@ var postcss = require('postcss');
 module.exports = postcss.plugin('postcss-advanced-variables', function (opts) {
 	opts = opts || {};
 
-	var matchDecl = /^\$([\w-]+)$/;
-	var matchVar = /\$(?:\(([\w-]+)\)|([\w-]+))/;
-	var parenWrap = /^\s*\((.*)\)\s*$/;
+	// Matchers
+	// --------
 
-	function getValueAsValueOrArray(string, node) {
-		string = transformVariables(string, node);
+	var isVariableDeclaration = /^\$[\w-]+$/;
+	var variablesInString = /(^|[^\\])\$(?:\(([A-z][\w-]*)\)|([A-z][\w-]*))/g;
+	var wrappingParen = /^\((.*)\)$/g;
 
-		return typeof string === 'string' && parenWrap.test(string) ? postcss.list.comma(string.replace(parenWrap, '$1')).map(function (innerstring) {
-			return getValueAsValueOrArray(innerstring);
-		}) : string;
+	// Helpers
+	// -------
+
+	// '(hello), (goodbye)' => [[hello], [goodbye]]
+	function getArrayedString(string, first) {
+		var array = postcss.list.comma(String(string)).map(function (substring) {
+			return wrappingParen.test(substring) ? getArrayedString(substring.replace(wrappingParen, '$1')) : substring;
+		});
+
+		return first && array.length === 1 ? array[0] : array;
 	}
 
+	// $NAME => VALUE
+	function getVariable(node, name) {
+		var value = node.variables && name in node.variables ? node.variables[name] : node.parent && getVariable(node.parent, name);
+
+		return value;
+	}
+
+	// node.variables[NAME] => 'VALUE'
 	function setVariable(node, name, value) {
-		name = name.replace(matchDecl, '$1');
-
-		if (parenWrap.test(value)) {
-			value = postcss.list.comma(value.replace(parenWrap, ''));
-		}
-
 		node.variables = node.variables || {};
 
-		node.variables[name.replace(matchDecl, '$1')] = value;
+		node.variables[name] = getArrayedString(value, true);
 	}
 
-	function getVariable(node, name) {
-		var variable = node.variables && name in node.variables ? node.variables[name] : node.parent && getVariable(node.parent, name);
+	// 'Hello $NAME' => 'Hello VALUE'
+	function getVariableTransformedString(node, string) {
+		return string.replace(variablesInString, function (match, before, name1, name2) {
+			var value = getVariable(node, name1 || name2);
 
-		return variable
+			return value === undefined ? match : before + value;
+		});
 	}
 
-	function transformVariables(string, node) {
-		while (string !== (string = string.replace(matchVar, function ($0, $1, $2) {
-			return getVariable(node, $1 || $2);
-		}))) {}
+	// Loopers
+	// -------
 
-		return string;
+	// run over every node
+	function each(parent) {
+		var index = -1;
+		var node;
+
+		while (node = parent.nodes[++index]) {
+			if (node.type === 'decl')        index = eachDecl  (node, parent, index);
+			else if (node.type === 'rule')   index = eachRule  (node, parent, index);
+			else if (node.type === 'atrule') index = eachAtRule(node, parent, index);
+
+			if (node.nodes) each(node);
+		}
 	}
 
+	// PROPERTY: VALUE
 	function eachDecl(node, parent, index) {
-		if (matchDecl.test(node.prop)) {
-			node.value = transformVariables(node.value, parent);
+		// $NAME: VALUE
+		if (isVariableDeclaration.test(node.prop)) {
+			node.value = getVariableTransformedString(parent, node.value);
 
-			setVariable(parent, node.prop, node.value);
+			setVariable(parent, node.prop.slice(1), node.value);
 
 			node.removeSelf();
 
 			--index;
-		} else {
-			node.prop = transformVariables(node.prop, parent);
-
-			node.value = transformVariables(node.value, parent);
 		}
+		else {
+			node.prop = getVariableTransformedString(parent, node.prop);
+
+			node.value = getVariableTransformedString(parent, node.value);
+		}
+
+		// return index
+		return index;
+	}
+
+	// SELECTOR {RULE}
+	function eachRule(node, parent, index) {
+		node.selector = getVariableTransformedString(parent, node.selector);
 
 		return index;
 	}
 
-	function eachAtForRule(node, parent, index) {
-		var params = postcss.list.space(node.params);
-
-		var iterator = params[0].slice(1);
-		var start    = +transformVariables(params[2], node);
-
-		setVariable(parent, iterator, start);
-
-		var end      = +transformVariables(params[4], node);
-		var dir      = end < start ? -1 : 1;
-		var by       = transformVariables(params[6] || 1, node) * dir;
-
-		for (; start * dir <= end * dir; start += by) {
-			setVariable(parent, iterator, start);
-
-			var clone = node.clone();
-
-			clone.parent = parent;
-
-			each(clone);
-
-			parent.insertBefore(node, clone.nodes);
-
-			++index;
-		}
-
-		node.removeSelf();
-
-		return --index;
-	}
-
-	function eachAtEachRule(node, parent, index) {
-		var params = node.params.split(' in ');
-
-		var iterator = params[0].slice(1);
-
-		var array = getValueAsValueOrArray(params.slice(1).join(' in '), parent);
-
-		var start = -1;
-		var end = array.length;
-
-		while (++start < end) {
-			setVariable(parent, iterator, array[start]);
-
-			var clone = node.clone();
-
-			clone.parent = parent;
-
-			each(clone);
-
-			parent.insertBefore(node, clone.nodes);
-
-			++index;
-		}
-
-		node.removeSelf();
-
-		return --index;
-	}
-
-	function eachAtIfRule(node, parent, index) {
-		var params = postcss.list.space(node.params);
-
-		var left = transformVariables(params[0], node);
-		var operator = params[1];
-		var right = transformVariables(params[2], node);
-
-		if (
-			(operator === '==' && left == right) ||
-			(operator === '!=' && left != right) ||
-			(operator === '<'  && left < right) ||
-			(operator === '<='  && left <= right) ||
-			(operator === '>'  && left > right) ||
-			(operator === '>='  && left >= right)
-		) {
-			parent.insertBefore(node, node.nodes);
-		}
-
-		node.removeSelf();
-
-		return --index;
-	}
-
+	// @NAME PARAMS
 	function eachAtRule(node, parent, index) {
 		if (node.name === 'for') index = eachAtForRule(node, parent, index);
 		else if (node.name === 'each') index = eachAtEachRule(node, parent, index);
@@ -152,20 +102,116 @@ module.exports = postcss.plugin('postcss-advanced-variables', function (opts) {
 		return index;
 	}
 
-	function eachRule(node, parent, index) {
-		node.selector = transformVariables(node.selector, parent);
+	// @for NAME from START to END by INCREMENT
+	function eachAtForRule(node, parent, index) {
+		// set params
+		var params = postcss.list.space(node.params);
 
-		return index;
+		var name      = params[0].trim().slice(1);
+		var start     = +getVariableTransformedString(node, params[2]);
+		var end       = +getVariableTransformedString(node, params[4]);
+		var increment = 6 in params && +getVariableTransformedString(node, params[6]) || 1;
+		var direction = start <= end ? 1 : -1;
+
+		// each iteration
+		while (start * direction <= end * direction) {
+			// set iterating variable
+			setVariable(node, name, start);
+
+			// clone node
+			var clone = node.clone({ parent: parent, variables: node.variables });
+
+			// process clone children
+			each(clone);
+
+			// increment index
+			index += clone.nodes.length;
+
+			// increment start
+			start += increment * direction;
+
+			// insert clone child nodes
+			parent.insertBefore(node, clone.nodes);
+		}
+
+		// remove node
+		node.removeSelf();
+
+		// return index
+		return --index;
 	}
 
-	function each(parent) {
-		for (var index = 0, node; (node = parent.nodes[index]); ++index) {
-			if (node.type === 'decl') index = eachDecl(node, parent, index);
-			else if (node.type === 'atrule') index = eachAtRule(node, parent, index);
-			else if (node.type === 'rule') index = eachRule(node, parent, index);
+	// @each NAME in ARRAY
+	function eachAtEachRule(node, parent, index) {
+		// set params
+		var params = node.params.split(' in ');
 
-			if (node.nodes) each(node);
+		var name  = params[0].trim().slice(1);
+		var array = getArrayedString(getVariableTransformedString(node, params.slice(1).join(' in ')), true);
+		var start = 0;
+		var end   = array.length;
+
+		// each iteration
+		while (start < end) {
+			// set iterating variable
+			setVariable(node, name, array[start]);
+
+			// clone node
+			var clone = node.clone({ parent: parent, variables: node.variables });
+
+			// process clone children
+			each(clone);
+
+			// increment index
+			index += clone.nodes.length;
+
+			// increment start
+			++start;
+
+			// insert clone child nodes
+			parent.insertBefore(node, clone.nodes);
 		}
+
+		// remove node
+		node.removeSelf();
+
+		// return index
+		return --index;
+	}
+
+	// @if LEFT OPERATOR RIGHT
+	function eachAtIfRule(node, parent, index) {
+		// set params
+		var params = postcss.list.space(node.params);
+
+		var left     = getVariableTransformedString(node, params[0]);
+		var operator = params[1];
+		var right    = getVariableTransformedString(node, params[2]);
+
+		// evaluate expression
+		if (
+			(operator === '==' && left == right) ||
+			(operator === '!=' && left != right) ||
+			(operator === '<'  && left < right) ||
+			(operator === '<='  && left <= right) ||
+			(operator === '>'  && left > right) ||
+			(operator === '>='  && left >= right)
+		) {
+			// process node children
+			each(node);
+
+			// increment index
+			index += node.nodes.length;
+
+			// insert child nodes
+			parent.insertBefore(node, node.nodes);
+		}
+
+		// remove node
+		node.removeSelf();
+
+		// return index
+		return --index;
 	}
 
 	return function (css) {
