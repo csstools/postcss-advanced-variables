d4ef30b49c27dbe8ac993021db5ea81697fd65aa
Jonathan Neal <jonathantneal@hotmail.com>
2018-01-05 10:33:20 -0500

    Update testing configuration

--- .rollup.js
+++ .rollup.js
@@ -3,11 +3,6 @@ import babel from 'rollup-plugin-babel';
 export default {
 	input: 'index.js',
 	output: { file: 'index.bundle.js', format: 'cjs' },
-	onwarn: (message) => {
-		if (!/external dependency/.test(message)) {
-			console.error(message);
-		}
-	},
 	plugins: [
 		babel({
 			plugins: [
